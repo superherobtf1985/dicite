73c649a4bbf667d26eef7cfd901ea94d3acb5df3
superherobtf1985 <shunto.ykk1210@gmail.com>
2019-09-01 00:14:27 +0000

    :bug: adminのitemsコントローラーの修復

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,8 +3,38 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
     @review = Review.new
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render :show
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:jacket_image, :name, :price, :stock, :status, :release_date, :artist_id, :label_id, :genre_id, cds_attributes: [:id, :number, :_destroy, songs_attributes: [:id, :name, :_destroy]])
   end
 end
