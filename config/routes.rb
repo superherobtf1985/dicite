7fe4ae171dab22468dc63daef56315bb5d5b9869
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-09-02 19:31:19 +0900

    :bug: レビュー内容を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hoge/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: :end_user do
     #   後で下を追加
---
+++ test/controllers/hoge_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class HogeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get hoge_index_url
+    assert_response :success
+  end
+
+end
