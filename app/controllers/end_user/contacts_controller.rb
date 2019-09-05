45cea15a70ffc0a0c2fcb7e642a89b4be9db7897
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-09-05 14:55:00 +0900

    :sparkles: お問い合わせフォームを作成

---
+++ app/controllers/end_user/contacts_controller.rb
@@ -0,0 +1,21 @@
+class EndUser::ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.user_id = current_user.id
+    if @contact.save
+      flash[:notice] = "ご意見ありがとうございました。"
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:subjects, :contacts)
+  end
+end
