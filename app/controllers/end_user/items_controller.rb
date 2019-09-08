a6d875d4dfb1e2385f7cd895e95612e71651ed9c
superherobtf1985 <shunto.ykk1210@gmail.com>
2019-09-08 07:47:22 +0000

    :bug: 検索機能のバグ修正

--- app/controllers/end_user/items_controller.rb
+++ app/controllers/end_user/items_controller.rb
@@ -2,13 +2,11 @@ class EndUser::ItemsController < ApplicationController
   def index
     @all_ranks = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(3).pluck(:item_id))
 
-    if Item.where(artist_id: params[:artist_id]).present?
+    @search = Item.ransack(params[:q])
+    @items = @search.result
+
+    if params[:artist_id].present?
       @items = Item.where(artist_id: params[:artist_id])
-    elsif Item.ransack(params[:q]).present?
-      @search = Item.ransack(params[:q])
-      @items = @search.result
-    else
-      @items = Item.all
     end
   end
 
